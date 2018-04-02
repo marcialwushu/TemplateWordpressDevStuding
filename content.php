c21b2168d5b7103d909dd95039a45e0d401e8bad
marcialwushu <marcialwushu@gmail.com>
2018-04-02 14:26:30 -0300

    get some parts od page - create content.php

---
+++ content.php
@@ -0,0 +1,12 @@
+<article>
+	<div class="post-image">
+		<div class="post-heading">
+			<h3><a href=""><?php The_title(); ?></a></h3>
+		</div>
+			<?php the_post_thumbnail(); ?>
+	</div>
+	<p>
+	    <?php the_content(); ?>
+	</p>
+						
+</article>
--- single.php
+++ single.php
@@ -24,18 +24,9 @@
 					
 			
 			?>
-				<article>
-						<div class="post-image">
-							<div class="post-heading">
-								<h3><a href=""><?php The_title(); ?></a></h3>
-							</div>
-							<?php the_post_thumbnail(); ?>
-						</div>
-						<p>
-							 <?php the_content(); ?>
-						</p>
-						
-				</article>
+
+				
+				<?php get_template_part('content'); ?>
 
 				<?php endwhile;
 				endif;
